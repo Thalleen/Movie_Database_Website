dd62e2dad24471863da59d50c868aeac184ff10b
Sumz-K <sumukhkowndinya@gmail.com>
2024-03-25 18:48:39 +0530

    Added DB operations

--- Movie db/src/Mongo_db.java
+++ Movie db/src/Mongo_db.java
@@ -1,6 +1,5 @@
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
@@ -8,13 +7,10 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.FindIterable;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
 import org.bson.Document;
-import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Mongo_db {
     String connectionString;
@@ -22,6 +18,7 @@ public class Mongo_db {
     MongoClientSettings settings;
     MongoDatabase database;
     MongoCollection<Document> collection;
+    MongoClient mongoClient;
 
     Mongo_db() {
         this.connectionString = "mongodb+srv://Sumukh:sumukh@cluster0.cpudzkw.mongodb.net/?retryWrites=true&w=majority";
@@ -32,20 +29,23 @@ public class Mongo_db {
                 .applyConnectionString(new ConnectionString(connectionString))
                 .serverApi(serverApi)
                 .build();
+        mongoClient = MongoClients.create(settings);
     }
 
 
     void connect(){
-        try(MongoClient mongoClient = MongoClients.create(settings)){
+        try{
             database = mongoClient.getDatabase("MovieDatabase");
             collection = database.getCollection("Movies");
-
+        }
+        catch (Exception e){
+            System.out.println("Error connecting to Mongo");
         }
     }
 
-    HashMap<String,ArrayList<String>> getall(){
+    HashMap<String, ArrayList<String>> getall() {
         FindIterable<Document> iterDoc = collection.find();
-
+        System.out.println("Retrieved");
         HashMap<String,ArrayList<String>>details=new HashMap<>();
         for(Document doc:iterDoc){
             String id=doc.get("_id").toString();
@@ -60,9 +60,40 @@ public class Mongo_db {
             temp.add(year);
             temp.add(rating);
             details.put(id, temp);
-
+            
         }
         return details;
+    }
+
+    HashMap<String, ArrayList<String>> getone(String movie){
+        FindIterable<Document> iterDoc = collection.find();
+        System.out.println("Retrieved");
+        HashMap<String,ArrayList<String>>details=new HashMap<>();
+        for(Document doc:iterDoc){
+            String id=doc.get("_id").toString();
+            String title=doc.get("Title").toString();
+            String genre=doc.get("Genre").toString();
+            String year=doc.get("ReleaseYear").toString();
+            String rating=doc.get("Rating").toString();
+
+
+            if(title.equals(movie)){
+                ArrayList<String>temp=new ArrayList<>();
+                temp.add(title);
+                temp.add(genre);
+                temp.add(year);
+                temp.add(rating);
+                details.put(id, temp);
+            }
+            
+        }
+        return details;
+    }
+
+
+    void writeMovie(JSONObject json){
+        Document document=new Document();
+        
     }
 
 
