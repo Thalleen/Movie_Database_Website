9fb15caca966f59f91886e61556276027ba77bf6
Sumz-K <sumukhkowndinya@gmail.com>
2024-03-25 15:39:15 +0530

    Added database funct

--- Movie db/src/Mongo_db.java
+++ Movie db/src/Mongo_db.java
@@ -7,9 +7,10 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
+import com.mongodb.client.FindIterable;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.bson.Document;
@@ -19,9 +20,11 @@ public class Mongo_db {
     String connectionString;
     ServerApi serverApi;
     MongoClientSettings settings;
+    MongoDatabase database;
+    MongoCollection<Document> collection;
 
     Mongo_db() {
-        this.connectionString = "mongodb+srv://Datanode_Manager:Datanode_Manager_Passcode@storage.rt8sdgl.mongodb.net/?retryWrites=true&w=majority&appName=Storage";
+        this.connectionString = "mongodb+srv://Sumukh:sumukh@cluster0.cpudzkw.mongodb.net/?retryWrites=true&w=majority";
         this.serverApi = ServerApi.builder()
                 .version(ServerApiVersion.V1)
                 .build();
@@ -31,44 +34,36 @@ public class Mongo_db {
                 .build();
     }
 
-    String insertIntoMongo(String Database, String dbCollection, Map<String, ArrayList<String>> dict, String filename,
-            String file_hash) {
-        String ret;
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase(Database);
-                MongoCollection<Document> collection = database.getCollection(dbCollection);
-                Document doc = new Document("Filename", filename);
-                doc.append("Hash", file_hash);
-                Document data = new Document();
-                try {
-                    fillin_data(data, dict);
-                } catch (Exception e) {
-                    // TODO: handle exception
-                    System.out.println("Error in fillin_data");
-                }
-                doc.append("data", data);
-                collection.insertOne(doc);
-                ret = "Success";
-                return ret;
-            } catch (MongoException e) {
-                //System.out.println("error in connecting to mongo database");
-                ret = "Error: " + e.getMessage();
-            }
-        } catch (MongoException e) {
-            ret = "Error: " + e.getMessage();
-        }
-        return ret;
-    }
-    private void fillin_data(Document obj, Map<String, ArrayList<String>> dict)
-            throws IOException, JSONException {
-        System.out.println("populating");
-        for (String key : dict.keySet()) {
-            String[] keys = key.split("-", 2);
-            Document doc1 = new Document(keys[1], dict.get(key));
-            obj.append(keys[0], doc1);
-        }
 
+    void connect(){
+        try(MongoClient mongoClient = MongoClients.create(settings)){
+            database = mongoClient.getDatabase("MovieDatabase");
+            collection = database.getCollection("Movies");
+
+        }
     }
+
+    HashMap<String,ArrayList<String>> getall(){
+        FindIterable<Document> iterDoc = collection.find();
+
+        HashMap<String,ArrayList<String>>details=new HashMap<>();
+        for(Document doc:iterDoc){
+            String id=doc.get("_id").toString();
+            String title=doc.get("Title").toString();
+            String genre=doc.get("Genre").toString();
+            String year=doc.get("ReleaseYear").toString();
+            String rating=doc.get("Rating").toString();
+
+            ArrayList<String>temp=new ArrayList<>();
+            temp.add(title);
+            temp.add(genre);
+            temp.add(year);
+            temp.add(rating);
+            details.put(id, temp);
+
+        }
+        return details;
+    }
+
+
 }
