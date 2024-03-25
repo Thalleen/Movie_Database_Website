b246b713e7a6974c38a3d3efa26621788a590866
Sumz-K <sumukhkowndinya@gmail.com>
2024-03-25 15:13:33 +0530

    Model

---
+++ Movie db/src/Model.java
@@ -0,0 +1,26 @@
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.SocketOption;
+import java.util.*;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Model{
+    
+}
